04e309fd16c47e90038e72bbff69dde14a2eb3fc
dortkthf <dortkthf@gmail.com>
2023-10-23 18:52:11 +0900

    thumbnail upload

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import { useDropzone } from 'react-dropzone';
 import Axios from 'axios';
+import {useSelector} from 'react-redux';
 
 const {TextArea} = Input
 const {Title} = Typography
@@ -18,12 +19,18 @@ const CategoryOptions = [
   {value:3, label:'Pets & Animals'},
 ]
 
-function VideoUploadPage() {
+function VideoUploadPage(props) {
+  const user = useSelector(state => state.user)
 
   const [VideoTitle, setVideoTitle] = useState('')
   const [Description, setDescription] = useState('')
   const [Private, setPrivate] = useState(0)
   const [Category, setCategory] = useState('Film & Animation')
+  const [FilePath, setFilePath] = useState('')
+  const [Duration, setDuration] = useState('')
+  const [ThumbnailPath, setThumbnailPath] = useState('')
+
+  
 
   const onTitleChange = (e) => {
     console.log(e.currentTarget.value)
@@ -52,6 +59,24 @@ function VideoUploadPage() {
     .then(response => {
       if(response.data.success) {
         console.log(response.data)
+
+        let variable = {
+          url : response.data.url,
+          fileName: response.data.fileName
+        }
+
+        setFilePath(response.data.url)
+
+        Axios.post('/api/video/thumbnail', variable)
+        .then(response => {
+          if(response.data.success){
+            console.log(response.data)
+            setDuration(response.data.fileDuration)
+            setThumbnailPath(response.data.url)
+          } else {
+            alert('썸네일 생성에 실패 했습니다.')
+          }
+        })
       } else {
         console.log(response.data)
         alert('비디오 업로드를 실패했습니다.')
@@ -68,12 +93,42 @@ function VideoUploadPage() {
     maxSize: 100000000000,
   });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const variable = {
+      writer: user.userData._id,
+      title: VideoTitle,
+      description: Description,
+      privacy: Private,
+      filePath: FilePath,
+      category: Category,
+      duration: Duration,
+      thumbnail: ThumbnailPath,
+    }
+
+    Axios.post('/api/video/uploadVideo', variable)
+    .then(response => {
+      if(response.data.success){
+        console.log(response.data)
+        message.success('성공적으로 업로드를 했습니다.')
+        
+        setTimeout(() => {
+          props.history.push('/')
+        }, 3000);
+
+      } else{
+        alert('비디오 업로드에 실패 했습니다.')
+      }
+    })
+  }
+
   return (
     <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
         <div style={{ textAlign:'center', marginBottom:'2rem' }}>
           <Title level={2}>Upload Video</Title>
         </div>
-        <Form onSubmit>
+        <Form onSubmit={onSubmit}>
           <div style={{ display:'flex', justifyContent:'space-between' }}>
             {/* Drop Zone */}
             <div {...getRootProps()} class='dropzone' >
@@ -84,9 +139,11 @@ function VideoUploadPage() {
                 </div>
             </div>
             {/* Thumbnail */}
-            <div>
-              <img src alt />
-            </div>
+            {ThumbnailPath &&
+              <div>
+                <img src={`http://localhost:5000/${ThumbnailPath}`} alt='thumbnail' />
+              </div>
+            }
           </div>
           <br/>
           <br/>
@@ -118,7 +175,7 @@ function VideoUploadPage() {
           </select>
           <br/>
           <br/>
-          <Button type='primary' size='large' >
+          <Button type='primary' size='large' onClick={onSubmit}>
             Submit
           </Button>
         </Form>
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-// const { Video } = require("../models/Video");
+const { Video } = require("../models/Video");
 const path = require('path')
 const { auth } = require("../middleware/auth");
 const multer = require('multer');
 const { error } = require('console');
+const ffmpeg = require('fluent-ffmpeg');
+
 
 const storage = multer.diskStorage({
     destination: (req,file,cb) => {
@@ -36,8 +38,56 @@ router.post('/uploadfiles', (req, res) => {
         if(err) {
             return res.status(400).json({success: false, error: err.msg});
         } else{
-            return res.json({success:true, filePath:res.req.file.path, fileName: res.req.file.filename})
+            return res.json({success:true, url:res.req.file.path, fileName: res.req.file.filename})
         }
+    })
+})
+
+router.post('/uploadVideo', (req, res) => {
+    // 비디오 정보를 저장한다.
+    const video = new Video(req.body)
+
+    video.save()
+    .then((doc)=> {
+        res.status(200).json({success:true})
+    })
+    .catch((err) => {
+        return res.json({success:false, err})
+    })
+})
+
+router.post('/thumbnail', (req, res) => {
+    // 썸네일 생성 하고 비디오 러닝타임도 가져오기
+
+    let filePath = ''
+    let fileDuration = ''
+    // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.url, function(err, metadata) {
+        console.dir(metadata);
+        console.log(metadata.format.duration);
+        fileDuration = metadata.format.duration
+    })
+    // 썸네일 생성
+    ffmpeg(req.body.url)
+    .on('filenames', function (filenames) {
+        console.log('will generate' + filenames.join(', '))
+        console.log(filenames)
+
+        filePath = 'uploads/thumbnails/' + filenames[0]
+    })
+    .on('end', function() {
+        console.log('screenshots taken');
+        return res.json({ success:true, url: filePath, fileDuration:fileDuration})
+    })
+    .on('error', function(err) {
+        console.error(err)
+        return res.json({success:false, err})
+    })
+    .screenshots({
+        count:3,
+        folder: 'uploads/thumbnails',
+        size:'320x240',
+        filename:'thumbnail-%b.png'
     })
 })
 
